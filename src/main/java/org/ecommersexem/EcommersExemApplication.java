8edd92556839401143e805d2e78b119d18346a3d
이주영 <141599648+ljy1348@users.noreply.github.com>
2024-12-02 20:35:30 +0900

    스프링 설치

---
+++ src/main/java/org/ecommersexem/EcommersExemApplication.java
@@ -0,0 +1,13 @@
+package org.ecommersexem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommersExemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommersExemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ecommersexem/EcommersExemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommersexem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommersExemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
